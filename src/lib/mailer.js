462ec7f340ad4bc8b66e6fda5e058d8c05daf1c2
bouabre225 <angebouabre463@gmail.com>
2026-01-08 01:53:02 +0100

    refactorisation du mailer pour s'adapter aux differents envoi de mail

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -1,6 +1,6 @@
-import nodemailer from "nodemailer"
-import fs from "node:fs/promises"
-import path from "node:path"
+import nodemailer from "nodemailer";
+import fs from "node:fs/promises";
+import path from "node:path";
 
 function hasSMTP() {
   return (
@@ -9,7 +9,7 @@ function hasSMTP() {
     process.env.MAIL_USER &&
     process.env.MAIL_PASS &&
     process.env.MAIL_FROM
-  )
+  );
 }
 
 const transporter = hasSMTP()
@@ -22,44 +22,58 @@ const transporter = hasSMTP()
         pass: process.env.MAIL_PASS,
       },
     })
-  : null
+  : null;
 
 async function renderTemplate(name, variables = {}) {
-  const filePath = path.join(
-    process.cwd(),
-    "src",
-    "templates",
-    `${name}.html`
-  )
-
-  let html = await fs.readFile(filePath, "utf-8")
+  const filePath = path.join(process.cwd(), "src", "lib", `${name}.html`);
+  let html = await fs.readFile(filePath, "utf-8");
 
   for (const [key, value] of Object.entries(variables)) {
-    html = html.replaceAll(`{{${key}}}`, value)
+    html = html.replaceAll(`{{${key}}}`, value);
   }
 
-  return html
+  return html;
 }
 
 export const mailer = {
-  async sendVerification(email, token) {
-    const front = process.env.FRONT_URL || "http://localhost:5173"
-    const verifyUrl = `${front}/verify-email?token=${token}`
-
+  async sendMail({ to, subject, html, text }) {
     if (!transporter) {
-      console.log(`[DEV] Verification link for ${email}: ${verifyUrl}`);
-      return
+      console.log(`[DEV] Mail to ${to} - ${subject}\n${text || html}`);
+      return;
     }
-
-    const html = await renderTemplate("verify-email", {
-      VERIFY_URL: verifyUrl,
-    });
 
     await transporter.sendMail({
       from: process.env.MAIL_FROM,
+      to,
+      subject,
+      text: text || html.replace(/<[^>]+>/g, ""), // fallback text
+      html,
+    });
+  },
+
+  async sendVerification(email, token) {
+    const front = process.env.FRONT_URL || "http://localhost:5173";
+    const verifyUrl = `${front}/verify-email?token=${token}`;
+    const html = await renderTemplate("verify-email", { VERIFY_URL: verifyUrl });
+
+    await this.sendMail({
       to: email,
       subject: "Confirme ton email",
       html,
-    })
+    });
   },
-}
+
+  async sendResetPassword(email, token) {
+    const front = process.env.FRONT_URL || "http://localhost:5173";
+    const resetUrl = `${front}/reset-password?token=${token}`;
+    const html = await renderTemplate("reset-password", { RESET_URL: resetUrl,
+    YEAR: new Date().getFullYear().toString()
+    });
+
+    await this.sendMail({
+      to: email,
+      subject: "Réinitialisation de votre mot de passe",
+      html,
+    });
+  },
+};
